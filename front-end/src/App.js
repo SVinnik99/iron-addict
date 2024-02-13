246e4c9a7420bec1daa1d2064db9b9d953735859
Samuel Vinnik <themansamuel3@gmail.com>
2024-02-13 13:12:43 -0600

    creating creat app, added a navbar

---
+++ front-end/src/App.js
@@ -0,0 +1,15 @@
+
+import Home from './components/Home/Index.js';
+import './App.css';
+
+function App() {
+  return (
+    <>
+
+      <Home/>
+      
+    </>
+  );
+}
+
+export default App;
---
+++ front-end/src/components/Home/Index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import HomeHeader from "./HomeNav";
+import HomeNav from "./HomeNav";
+import "./Index.css"
+
+function Home() {
+  return (
+    <>
+      <HomeNav />
+
+      <h1>Iron Addict</h1>
+    </>
+  );
+}
+
+export default Home;
